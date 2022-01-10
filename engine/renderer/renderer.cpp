ece0a6535467c5ceca6520e539b4c9cb3dbf0ec2
notSanil <44319106+notSanil@users.noreply.github.com>
2022-01-10 13:36:43 +0530

    split renderer into cpp and hpp

--- engine/renderer/renderer.cpp
+++ engine/renderer/renderer.cpp
@@ -1,3 +1,21 @@
 #include "renderer.hpp"
 
+
+Renderer::Renderer(){};
+
+void Renderer::init(SDL_Window *window){
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer){
+        throw SDLException(Renderer_Failed);
+    }
+}
+
+SDL_Renderer* Renderer::get(){
+    return renderer;
+}
+
+void Renderer::destroy(){
+    SDL_DestroyRenderer(renderer);
+    renderer = nullptr;
+}
 SDL_Renderer* Renderer::renderer = nullptr;
--- engine/renderer/renderer.hpp
+++ engine/renderer/renderer.hpp
@@ -6,23 +6,11 @@
 class Renderer{
 private:
     static SDL_Renderer* renderer;
-    Renderer(){};
+    Renderer();
 
 public:
-    static void init(SDL_Window *window){
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if (!renderer){
-            throw SDLException(Renderer_Failed);
-        }
-    }
-
-    static SDL_Renderer* get(){
-        return renderer;
-    }
-
-    static void destroy(){
-        SDL_DestroyRenderer(renderer);
-        renderer = nullptr;
-    }
+    static void init(SDL_Window *window);
+    static SDL_Renderer* get();
+    static void destroy();
 };
 
